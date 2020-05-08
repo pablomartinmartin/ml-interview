32c4326669017e669bedeabb4375664e49581cfc
pmartin-eb <pmartin@eventbrite.com>
2020-05-07 21:15:04 -0300

    finished endpoints

--- server/server.js
+++ server/server.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const request = require("request");
+const Promise = require("promise");
 const querystring = require("querystring");
-const { find } = require("lodash");
+const { find, first } = require("lodash");
 const app = express();
 const port = 3001;
 
 const API_BASE_URL = "https://api.mercadolibre.com";
+const AUTHOR_DATA = {
+  author: {
+    name: "Pablo",
+    lastname: "Martin",
+  },
+};
+
+const getItems = (URL_REQUEST) => {
+  var options = {
+    url: URL_REQUEST,
+    headers: {
+      "User-Agent": "request",
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    request.get(options, function (err, resp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+};
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -28,9 +53,9 @@ app.get("/api/items", (req, res, next) => {
       id: item.id,
       title: item.title,
       price: {
-        amount: String(item.price).split(".")[0] || item.price,
+        amount: Number(String(item.price).split(".")[0]) || item.price,
         currency: item.currency_id,
-        decimals: String(item.price).split(".")[1] || "",
+        decimals: Number(String(item.price).split(".")[1]) || "",
       },
       picture: item.thumbnail,
       free_shipping: item.shipping.free_shipping,
@@ -38,15 +63,50 @@ app.get("/api/items", (req, res, next) => {
     }));
 
     const itemsResults = {
-      author: {
-        name: "Pablo",
-        lastname: "Martin",
-      },
+      author: AUTHOR_DATA.author,
       items: formattedResults,
       categories,
     };
 
     //   res.status('200').json(itemsResults)
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(itemsResults));
+  });
+});
+
+app.get("/api/items/:id", (req, res, next) => {
+  const URL_ITEMS_BY_ID = `${API_BASE_URL}/items/${req.params.id}`;
+  const URL_ITEMS_DESCRIPTION = `${API_BASE_URL}/items/${req.params.id}/description`;
+
+  const itemsByIdPromise = getItems(URL_ITEMS_BY_ID);
+  const descriptionByIdPromise = getItems(URL_ITEMS_DESCRIPTION);
+
+  const batchItems = Promise.all([itemsByIdPromise, descriptionByIdPromise]);
+
+  batchItems.then((results) => {
+    const productData = first(results);
+    const productPlainDescription = results[1].plain_text;
+
+    const formattedItem = {
+      id: productData.id,
+      title: productData.title,
+      price: {
+        currency: productData.currency_id,
+        amount: Number(String(productData.price).split(".")[0]) || productData.price,
+        decimals: Number(String(productData.price).split(".")[1]) || "",
+      },
+      picture: first(productData.pictures).url,
+      condition: productData.condition,
+      free_shipping: productData.shipping.free_shipping,
+      sold_quantity: productData.sold_quantity,
+      description: productPlainDescription,
+    };
+
+    const itemsResults = {
+      author: AUTHOR_DATA.author,
+      item: formattedItem,
+    };
+
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(itemsResults));
   });
